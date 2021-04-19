b48b776f929f6d1bfce79b70c461bae74caf66d1
Yewon-dev <kyw20160785@gmail.com>
2021-04-19 22:43:16 +0900

    다이나믹 프로그래밍 연습문제2

--- lec6-4.py
+++ lec6-4.py
@@ -12,14 +12,12 @@ for i in range(n):
 # 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
 d = [10001] * (m + 1) # m원까지 결과를 구하기 위해
 
-
 # 다이나믹 프로그래밍 (보텀업)
 d[0] = 0
 for i in range(n): # i : 각각의 화폐 단위
     for j in range(array[i], m + 1): # j : 금액 # 화폐단위원부터 m원까지 확인
         if d[j - array[i]] != 10001: # (i - k)원을 만드는 방법이 존재하는 경우 == 금액에서 화폐단위를 뺄 수 있다면
             d[j] = min(d[j], d[j - array[i]] + 1)
-
 
 if d[m] == 10001:
     print(-1)

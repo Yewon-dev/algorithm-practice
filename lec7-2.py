2dad5a367d1166b067c9acd27588cd3eb9c732f8
Yewon-dev <kyw20160785@gmail.com>
2021-04-20 17:49:52 +0900

    플로이드 워셜 알고리즘

---
+++ lec7-2.py
@@ -0,0 +1,32 @@
+# 플로이드 워셜 알고리즘
+# 2차원 테이블에 최단 거리 정보 저장
+# 노드가 적은 경우 효과적 -> 시간복잡도 O(n^3)
+
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+    
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for k in range(1, n + 1):
+    for a in range(1, n + 1):
+        for b in range(1, n + 1):
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+    
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
